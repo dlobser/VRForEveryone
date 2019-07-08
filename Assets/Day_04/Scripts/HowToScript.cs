0ff7dd9cec5513380a87a5413e24828a163a2969
david lobser <davidlobser@yahoo.com>
2019-07-08 16:02:42 -0400

    added lots of assets

---
+++ Assets/Day_04/Scripts/HowToScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToScript : MonoBehaviour
+{
+    public int integerValue;
+    public float floatValue;
+    public float secondFloatValue;
+
+    public string stringValue;
+    public Vector3 vectorValue;
+
+    //This is a function, everything inside it gets called on start
+    void Start()
+    {
+        
+    }
+
+    //This function is called automatically every frame
+    //It says "void" because it doesn't return anything
+    void Update()
+    {
+        Debug.Log("Integers are whole numbers, such as: " + integerValue + "\n");
+        Debug.Log("Floating point numbers contain decimal points, such as: " + floatValue + "\n");
+        Debug.Log("Strings are letters, words and sentences: " + stringValue + "\n");
+        Debug.Log("Vectors are special objects which contain 2, 3, or 4 floats: " + vectorValue + "\n");
+        Debug.Log("Functions can do things like add numbers together: " + Add(floatValue,secondFloatValue) + "\n");
+
+    }
+
+    //This function says float because it returns the sum of two floats we give it
+    float Add(float A, float B){
+        return A + B;
+    }
+}
---
+++ Assets/_Master/Scripts/Interaction/Interactable_Destroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_Destroy : Interactable
+{
+    //explodes an object when clicked
+
+    public GameObject explosion;
+
+    public override void HandleHover()
+    {
+        if(clicked>.5f){
+            HandleTrigger();
+        }
+    }
+
+	public override void HandleTrigger()
+	{
+		base.HandleTrigger();
+        GameObject e = Instantiate(explosion);
+        e.transform.position = this.transform.position;
+        Destroy(this.gameObject);
+	}
+}
